798a8f904d2892a0a6db2fb034dee538f7b0db2b
Cameron Brouk <cameronbrouk@gmail.com>
2022-02-26 17:05:53 -0600

    SETUP ESLINT: integrate eslint with vite

--- apps/vite-app/vite.config.ts
+++ apps/vite-app/vite.config.ts
@@ -1,9 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import eslint from 'vite-plugin-eslint'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
+    eslint({
+      fix: true,
+      cache: true,
+      throwOnError: true,
+    }),
     react({
       include: '**/*.tsx',
       exclude: ['**/*.(spec|test).*'],
