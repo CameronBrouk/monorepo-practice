9fbe1c480f656e76b0193f29cf0c8a86f9d870ad
Cameron Brouk <cameronbrouk@gmail.com>
2022-02-26 21:35:08 -0600

    Add code to App.tsx to test

--- apps/vite-app/src/App.tsx
+++ apps/vite-app/src/App.tsx
@@ -4,13 +4,12 @@ export function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <div>
+    <div className='max-w-full w-screen h-screen max-h-screen bg-gray-200 text-red-800 flex justify-center items-center text-xl'>
       <header>
-        <p>
-          <button type='button' onClick={() => setCount((count) => count + 1)}>
-            count is: {count}
-          </button>
-        </p>
+        <span className='flex space-x-4'>
+          <button onClick={() => setCount((count) => count + 1)}>count</button>
+          <p>{count} + 1</p>
+        </span>
         <p>
           Edit <code>App.tsx</code> and save to test HMR updates.
         </p>
